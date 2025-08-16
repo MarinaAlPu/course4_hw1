ecaf41b9d85e29a52faf9682db24e53fecf31ad3
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-08-16 17:46:39 +0500

    Add function to formate date

--- modules/helpers.js
+++ modules/helpers.js
@@ -22,5 +22,17 @@ function formatText(text) {
   return text.replaceAll("<", "&lt").replaceAll(">", "&gt");
 }
 
+function formateDate(dateFromServer) {
+  const dateOptions = { day: '2-digit', month: '2-digit', year: 'numeric' };
+  const timeOptions = { hour: 'numeric', minute: 'numeric' };
 
-export { validation, deleteClass, setPlaceholder, formatText }
+  const date = (new Date(dateFromServer)).toLocaleDateString('ru-RU', dateOptions);
+  const time = (new Date(dateFromServer)).toLocaleTimeString('ru-RU', timeOptions);
+
+  const dateForComment = date + " " + time;
+
+  return dateForComment;
+}
+
+
+export { validation, deleteClass, setPlaceholder, formatText, formateDate }
