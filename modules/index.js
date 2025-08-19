58bfad2703aa39ae503e5db8008cddf5d7439cfd
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-08-19 09:11:43 +0500

    Add loader for adding comment

--- modules/index.js
+++ modules/index.js
@@ -8,10 +8,10 @@ import { normalizeData, delay } from "./helpers.js";
 const nameInput = document.getElementById('name');
 const commentInput = document.getElementById('commentText');
 const addCommentButton = document.querySelector('.add-form-button');
-
 const commentsList = document.querySelector('ul.comments');
 const loaderComments = document.querySelector('.loader-comments');
 const loaderComment = document.querySelector('.loader-comment');
+const form = document.querySelector('.add-form')
 
 
 nameInput.addEventListener("click", () => {
@@ -31,19 +31,22 @@ commentInput.addEventListener("blur", () => {
 });
 
 loaderComments.style.display = "block";
-// loaderComment.style.display = "none";
+loaderComment.style.display = "none";
 commentsList.style.display = "none";
 
 addCommentButton.addEventListener("click", () => {
-
+  loaderComment.style.display = "block";
+  form.style.display = "none";
   addComment();
   delay(2000).then(() => {
     getComments()
       .then((data) => {
         const normalizeСomments = normalizeData(data);
         updateComments(normalizeСomments);
-
+        loaderComment.style.display = "none";
+        form.style.display = "block";
         renderComments();
+
       })
   });
 });
