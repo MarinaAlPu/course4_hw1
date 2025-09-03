08c102f1705a7e5a85e8a834a18fe393bd5d7b79
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-09-03 08:54:11 +0500

    Add error handling if status code 401

--- modules/api.js
+++ modules/api.js
@@ -1,29 +1,62 @@
+const baseUrl = "https://wedev-api.sky.pro/api/v2/marina-pudovkina";
+const authUrl = "https://wedev-api.sky.pro/api/user";
+
+let token = "bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck";
+
+
 const getComments = () => {
-  return fetch("https://wedev-api.sky.pro/api/v1/marina-pudovkina/comments", {
+  return fetch(`${baseUrl}/comments`, {
     method: 'GET',
   })
     .then((response) => {
+      // console.log(response);
+      // console.log(response.status);
+
       if (response.status === 200) {
         return response.json();
-      } else if (response.status === 404) {
-        throw new Error("Страница не найдена");
-      } else if (response.status === 500) {
-        throw new Error("Сервер сломался, попробуй позже");
+        // } else if (response.status === 401) {
+        //   throw new Error("Вы не авторизовались");
+        // } else if (response.status === 404) {
+        //   throw new Error("Страница не найдена");
+        // } else if (response.status === 500) {
+        //   throw new Error("Сервер сломался, попробуй позже");
+        // } else {
+        //   throw new Error(`Неопознанная ошибка. Статус код ${response.status}`);
+        // }
       } else {
+        // if (response.status === 401) {
+        //   throw new Error("Вы не авторизовались");
+        // }
+
+        if (response.status === 404) {
+          throw new Error("Страница не найдена");
+        }
+
+        if (response.status === 500) {
+          throw new Error("Сервер сломался, попробуй позже");
+        }
+
         throw new Error(`Неопознанная ошибка. Статус код ${response.status}`);
       }
     })
 }
 
 const sendComment = (text, name) => {
-  return fetch("https://wedev-api.sky.pro/api/v1/marina-pudovkina/comments", {
+  return fetch(`${baseUrl}/comments`, {
     method: 'POST',
     body: JSON.stringify({ "text": text, "name": name, forceError: true })
   })
     .then((response) => {
+      // console.log(response);
+      // console.log("\nКод ответа: ", response.status);
+
       if (response.status === 201) {
         response.json();
       } else {
+        if (response.status === 401) {
+          throw new Error("Вы не авторизовались");
+        }
+
         if (response.status === 400) {
           throw new Error("Имя и комментарий должны быть не короче 3 символов");
         }
@@ -41,5 +74,41 @@ const sendComment = (text, name) => {
     })
 }
 
+const login = (login, password) => {
+  return fetch(`${authUrl}/login`, {
+    method: POST,
+    body: JSON.stringify({
+      login,
+      password
+    })
+  })
+    .then((response) => {
+      return response.json()
+    })
+}
 
-export { getComments, sendComment }
+const registration = (login, name, password) => {
+  return fetch(authUrl, {
+    method: POST,
+    body: JSON.stringify({
+      login,
+      name,
+      password
+    })
+  })
+    .then((response) => {
+      return response.json()
+    })
+}
+
+const getUsers = () => {
+  return fetch(authUrl, {
+    method: GET
+  })
+    .then((response) => {
+      return response.json()
+    })
+}
+
+
+export { getComments, sendComment, login, registration, getUsers }
